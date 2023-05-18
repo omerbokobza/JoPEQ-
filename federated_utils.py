e8574fae8f895b3ba5fa4c998fcac2c4c7323389
omerbokobza <119860235+omerbokobza@users.noreply.github.com>
2023-05-18 17:52:52 +0300

    Add files via upload

--- federated_utils.py
+++ federated_utils.py
@@ -40,10 +40,12 @@ def aggregate_models(local_models, global_model, mechanism):  # FeaAvg
         SNR_users = []
         for user_idx in range(0, len(local_models)):
             local_weights_orig = local_models[user_idx]['model'].state_dict()[key] - state_dict[key]
+            local_weights_orig = local_weights_orig
             local_weights = mechanism(local_weights_orig)
             SNR_users.append(torch.var(local_weights_orig) / torch.var(local_weights_orig - local_weights))
             local_weights_average += local_weights
         SNR_layers.append(mean(SNR_users))
+        state_dict[key] = state_dict[key]
         state_dict[key] += (local_weights_average / len(local_models)).to(state_dict[key].dtype)
     global_model.load_state_dict(copy.deepcopy(state_dict))
 
@@ -53,7 +55,7 @@ def aggregate_models(local_models, global_model, mechanism):  # FeaAvg
 
 class JoPEQ:  # Privacy Quantization class
     def __init__(self, args, alpha = 0.6, hex_mat = torch.tensor([[np.sqrt(3) / 2, 0], [1 / 2, 1]]).to(torch.float32)):
-        hex_mat = torch.eye(2).to(torch.float32)
+        hex_mat = torch.eye(args.lattice_dim).to(torch.float32)
         # hex_mat = (torch.tensor([[0.0618, 0.98], [0.98, 0.043]])).to(torch.float32)
         self.vec_normalization = args.vec_normalization
         self.alpha = alpha
@@ -77,10 +79,11 @@ class JoPEQ:  # Privacy Quantization class
 
     def divide_into_blocks(self, input, dim=2):
         # Zero pad if needed
+        input = input.view(-1)
         modulo = len(input) % dim
         if modulo:
             pad_with = dim - modulo
-            input_vec = torch.cat((input, torch.zeros(pad_with).to(input.dtype).to(input.device)))
+            input = torch.cat((input, torch.zeros(pad_with).to(input.dtype).to(input.device)))
         else:
             pad_with = 0
         input_vec = input.view(dim, -1)  # divide input into blocks
@@ -88,22 +91,21 @@ class JoPEQ:  # Privacy Quantization class
 
     def __call__(self, input):
         original_shape = input.shape
-        input = input.view(-1)
         if self.vec_normalization:  # normalize
-            input, pad_with = self.divide_into_blocks(input)
-
+            input, pad_with = self.divide_into_blocks(input, self.args.lattice_dim)
         mean = torch.mean(input, dim=-1, keepdim=True)
         std = torch.norm(input - mean) / (input.shape[-1] ** 0.5)
 
-        std = self.alpha  * std    ## was 3 * std
+        std = self.alpha * std    ## was 3 * std
 
         input = (input - mean) / std
-
+        # print(f"input:{input.shape}, mean:{mean.shape}, std:{std.shape}")
         if self.privacy is not None:
             input = self.privacy(input)
 
         if self.quantizer is not None:
             input = self.quantizer(input)
+        # print(f"input:{input.shape}, mean:{mean.shape}, std:{std.shape}")
 
         # denormalize
         input = (input * std) + mean
@@ -112,5 +114,5 @@ class JoPEQ:  # Privacy Quantization class
             input = input.view(-1)[:-pad_with] if pad_with else input  # remove zero padding
 
         input = input.reshape(original_shape)
-
+        # print(f"at the end:{input.shape}")
         return input
--- main.py
+++ main.py
@@ -13,97 +13,105 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    args = args_parser()
-    boardio, textio, best_val_acc, path_best_model = utils.initializations(args)
-    textio.cprint(str(args))
+    for r in [2,4,6]:
+        for dim in [2,3,4]:
+            start_time = time.time()
+            args = args_parser()
+            args.model = 'linear'
+            args.R = r
+            args.privacy = False
+            args.alpha = 1
+            args.lattice_dim = dim
 
-    # data
-    train_data, test_loader = utils.data(args)
-    input, output, train_data, val_loader = utils.data_split(train_data, len(test_loader.dataset), args)
+            boardio, textio, best_val_acc, path_best_model = utils.initializations(args)
+            textio.cprint(str(args))
 
-    # model
-    if args.model == 'mlp':
-        global_model = models.FC2Layer(input, output)
-    elif args.model == 'cnn2':
-        global_model = models.CNN2Layer(input, output, args.data)
-    elif args.model == 'cnn3':
-        global_model = models.CNN3Layer()
-    else:
-        global_model = models.Linear(input, output)
-    print(str(summary(global_model)))
+            # data
+            train_data, test_loader = utils.data(args)
+            input, output, train_data, val_loader = utils.data_split(train_data, len(test_loader.dataset), args)
 
-    textio.cprint(str(summary(global_model)))
-    global_model.to(args.device)
+            # model
+            if args.model == 'mlp':
+                global_model = models.FC2Layer(input, output)
+            elif args.model == 'cnn2':
+                global_model = models.CNN2Layer(input, output, args.data)
+            elif args.model == 'cnn3':
+                global_model = models.CNN3Layer()
+            else:
+                global_model = models.Linear(input, output)
+            print(str(summary(global_model)))
 
-    train_creterion = torch.nn.CrossEntropyLoss(reduction='mean')
-    test_creterion = torch.nn.CrossEntropyLoss(reduction='sum')
+            textio.cprint(str(summary(global_model)))
+            global_model.to(args.device)
 
-    # learning curve
-    train_loss_list = []
-    val_acc_list = []
-    overloading_list = []
-    avg_SNR_list = []
+            train_creterion = torch.nn.CrossEntropyLoss(reduction='mean')
+            test_creterion = torch.nn.CrossEntropyLoss(reduction='sum')
 
-    #  inference
-    if args.eval:
-        global_model.load_state_dict(torch.load(path_best_model))
-        test_acc = utils.test(test_loader, global_model, args.device)
-        textio.cprint(f'eval test_acc: {test_acc:.0f}%')
-        gc.collect()
-        sys.exit()
+            # learning curve
+            train_loss_list = []
+            val_acc_list = []
+            overloading_list = []
+            avg_SNR_list = []
 
-    # training loops
-    local_models = federated_utils.federated_setup(global_model, train_data, args)
-    mechanism = federated_utils.JoPEQ(args, args.alpha)
+            #  inference
+            if args.eval:
+                global_model.load_state_dict(torch.load(path_best_model))
+                test_acc = utils.test(test_loader, global_model,test_creterion, args.device)
+                textio.cprint(f'eval test_acc: {test_acc:.0f}%')
+                gc.collect()
+                sys.exit()
 
-    SNR_list = []
+            # training loops
+            local_models = federated_utils.federated_setup(global_model, train_data, args)
+            mechanism = federated_utils.JoPEQ(args, args.alpha)
 
-    for global_epoch in tqdm(range(0, args.global_epochs)):
-        federated_utils.distribute_model(local_models, global_model)
-        users_loss = []
+            SNR_list = []
 
-        for user_idx in range(args.num_users):
-            user_loss = []
-            for local_epoch in range(0, args.local_epochs):
-                user = local_models[user_idx]
-                train_loss = utils.train_one_epoch(user['data'], user['model'], user['opt'],
-                                                   train_creterion, args.device, args.local_iterations)
-                if args.lr_scheduler:
-                    user['scheduler'].step(train_loss)
-                user_loss.append(train_loss)
-            users_loss.append(mean(user_loss))
+            for global_epoch in tqdm(range(0, args.global_epochs)):
+                federated_utils.distribute_model(local_models, global_model)
+                users_loss = []
 
-        train_loss = mean(users_loss)
-        SNR = federated_utils.aggregate_models(local_models, global_model, mechanism)  # FeaAvg
-        SNR_list.append(SNR)
+                for user_idx in range(args.num_users):
+                    user_loss = []
+                    for local_epoch in range(0, args.local_epochs):
+                        user = local_models[user_idx]
+                        train_loss = utils.train_one_epoch(user['data'], user['model'], user['opt'],
+                                                           train_creterion, args.device, args.local_iterations)
+                        if args.lr_scheduler:
+                            user['scheduler'].step(train_loss)
+                        user_loss.append(train_loss)
+                    users_loss.append(mean(user_loss))
 
-        val_acc = utils.test(val_loader, global_model, test_creterion, args.device)
+                train_loss = mean(users_loss)
+                SNR = federated_utils.aggregate_models(local_models, global_model, mechanism)  # FeaAvg
+                SNR_list.append(SNR)
 
-        train_loss_list.append(train_loss)
-        val_acc_list.append(val_acc)
+                val_acc = utils.test(val_loader, global_model, test_creterion, args.device)
 
-        boardio.add_scalar('train', train_loss, global_epoch)
-        boardio.add_scalar('validation', val_acc, global_epoch)
-        gc.collect()
+                train_loss_list.append(train_loss)
+                val_acc_list.append(val_acc)
 
-        if val_acc > best_val_acc:
-            best_val_acc = val_acc
-            torch.save(global_model.state_dict(), path_best_model)
+                boardio.add_scalar('train', train_loss, global_epoch)
+                boardio.add_scalar('validation', val_acc, global_epoch)
+                gc.collect()
 
-        if type(mechanism) == federated_utils.JoPEQ:
-            overloading = mechanism.quantizer.print_overloading_vec()
-            overloading_list.append(overloading)
-        avg_SNR_list.append(20 * torch.log10(sum(SNR_list) / len(SNR_list)))
-        textio.cprint(f'epoch: {global_epoch} | train_loss: {train_loss:.2f} | '
-                      f'val_acc: {val_acc:.0f}% | '
-                      f'SNR: {20 * torch.log10(SNR):.3f} | '
-                      f'avg SNR: {20 * torch.log10(sum(SNR_list) / len(SNR_list)):.3f} | '
-                      f'OL: {overloading:.4f}%')
+                if val_acc > best_val_acc:
+                    best_val_acc = val_acc
+                    torch.save(global_model.state_dict(), path_best_model)
 
-    np.save(f'checkpoints/{args.exp_name}/train_loss_list.npy', train_loss_list)
-    np.save(f'checkpoints/{args.exp_name}/val_acc_list.npy', val_acc_list)
-    elapsed_min = (time.time() - start_time) / 60
-    textio.cprint(f'total execution time: {elapsed_min:.0f} min')
-    # textio.cprint(f'The overloading vector is: {overloading_list}')
-    # textio.cprint(f'The AVG SNR vector is: {avg_SNR_list}')
+                if type(mechanism) == federated_utils.JoPEQ:
+                    overloading = mechanism.quantizer.print_overloading_vec()
+                    overloading_list.append(overloading)
+                avg_SNR_list.append(20 * torch.log10(sum(SNR_list) / len(SNR_list)))
+                textio.cprint(f'epoch: {global_epoch} | train_loss: {train_loss:.2f} | '
+                              f'val_acc: {val_acc:.0f}% | '
+                              f'SNR: {20 * torch.log10(SNR):.3f} | '
+                              f'avg SNR: {20 * torch.log10(sum(SNR_list) / len(SNR_list)):.3f} | '
+                              f'OL: {overloading:.4f}%')
+
+            np.save(f'checkpoints/{args.exp_name}/train_loss_list.npy', train_loss_list)
+            np.save(f'checkpoints/{args.exp_name}/val_acc_list.npy', val_acc_list)
+            elapsed_min = (time.time() - start_time) / 60
+            textio.cprint(f'total execution time: {elapsed_min:.0f} min')
+            # textio.cprint(f'The overloading vector is: {overloading_list}')
+            # textio.cprint(f'The AVG SNR vector is: {avg_SNR_list}')
